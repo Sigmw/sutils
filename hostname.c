a8add1cb617c0cf242ab25ef3129b6dacdcbcf78
Sigma <sigmw@ctemplar.com>
2022-04-30 22:40:49 -0300

    fmt

--- hostname.c
+++ hostname.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[]) {
-	char host[256];
-	switch(argc) {
-		case 1:
-		gethostname(host, sizeof(host));
-		printf("%s\n", host);
-		break;
-		case 2:
-		if(sethostname(argv[1], strlen(argv[1]))) {
-			perror("error");
-		}
-		break;
-		default:
-		printf("usage: hostname[host]\n");
-		_exit(1);
-		break;
-	}
-	
-
+int main(int argc, char *argv[]) {
+  char host[256];
+  switch (argc) {
+  case 1:
+    gethostname(host, sizeof(host));
+    printf("%s\n", host);
+    break;
+  case 2:
+    if (sethostname(argv[1], strlen(argv[1]))) {
+      perror("error");
+    }
+    break;
+  default:
+    printf("usage: hostname[host]\n");
+    _exit(1);
+    break;
+  }
 }
