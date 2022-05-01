2307317a99199c9675a43c18a556e68ea0ad6e7c
Sigma <sigmw@ctemplar.com>
2022-04-30 21:17:58 -0300

    clang fmt

--- echo.c
+++ echo.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char* argv[]) {
-	if(argc < 2) exit(0);
-	int flag = strcmp(argv[1], "-n");
-	int i = flag ? 1 : 2;
-	for(; i < argc; i++) {
-		printf("%s ", argv[i]);
-	}
-	if(flag) putchar('\n');
+int main(int argc, char *argv[]) {
+  if (argc < 2)
+    exit(0);
+  int flag = strcmp(argv[1], "-n");
+  int i = flag ? 1 : 2;
+  for (; i < argc; i++) {
+    printf("%s ", argv[i]);
+  }
+  if (flag)
+    putchar('\n');
 }
